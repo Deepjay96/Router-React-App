61bfcce6af37524fcc9722c32f2fe149e741438d
Deepjay96 <jaydipgirase65904@gmail.com>
2024-03-26 19:37:13 +0530

    this is route project in react

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import Home from "./components/Home";
+
+function App() {
+ 
+
+  return (
+    <>
+      
+      <Header></Header>
+      <Home></Home>
+      <Footer></Footer>
+    </>
+  );
+}
+
+export default App;
